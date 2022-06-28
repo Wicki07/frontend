57a0fe260df0e349935034075f4c19a9448381a9
Mateusz Wicki <mateuszwicki1@gmail.com>
2022-06-28 20:01:38 +0200

    update

--- dist/js/chunk-2d0f0bc5.afc9c2a6.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0bc5"],{"9e47":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],r={},l=r,i=t("2877"),o=Object(i["a"])(l,u,c,!1,null,null,null);n["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d0f0bc5.afc9c2a6.js.map
